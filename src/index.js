d7a5aaa0e76a9735a6fa732cc82229e65e06b498
talencote <vitkomax01@gmail.com>
2020-10-22 01:41:29 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -38,8 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let decodedMessage = "";
+    let undecodedLetter = "";
+    for (let i = 0; i < expr.length; i+=10) {
+        undecodedLetter = expr.substr(i,10);
+        if(undecodedLetter == '**********'){
+            decodedMessage+=" ";
+        }
+       else {
+           decodedMessage+=findLetter(undecodedLetter);
+       }
+    }
+      return decodedMessage;
+  }
+
+function findLetter(undecodedLetter){
+    let binarySymbol;
+    let decodedLetter;
+    let morseLetter="";
+    for(let i = 0; i < 10; i+=2){
+        binarySymbol=undecodedLetter.substr(i,2);
+        if (binarySymbol == "10"){
+            morseLetter+='.';
+        }
+        else if (binarySymbol == "11"){
+            morseLetter+="-";
+        }
+    }
+    decodedLetter = MORSE_TABLE[morseLetter];
+    return decodedLetter;
 }
+
 
 module.exports = {
     decode
